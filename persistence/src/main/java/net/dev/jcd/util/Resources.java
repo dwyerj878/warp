b5c8a6a0f9529ab8d041c48ca4d8f2c7b3402f10
dwyerj878 <jcdwyer@jcd-dev.net>
2014-09-11 18:00:37 -0500

    all this bits are functional

--- persistence/src/main/java/net/dev/jcd/util/Resources.java
+++ persistence/src/main/java/net/dev/jcd/util/Resources.java
@@ -18,10 +18,8 @@ package net.dev.jcd.util;
 
 import java.util.logging.Logger;
 
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -49,10 +47,6 @@ public class Resources {
         return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
     }
 
-    @Produces
-    @RequestScoped
-    public FacesContext produceFacesContext() {
-        return FacesContext.getCurrentInstance();
-    }
+
 
 }
