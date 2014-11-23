77c4fd43130e665254260884ff1a1b15a16a66be
jcdwyer <jcdwyer@jcd-dev.net>
2014-11-23 09:39:08 -0600

    hooked up to services

--- services/src/main/webapp/js/app.js
+++ services/src/main/webapp/js/app.js
@@ -1,9 +1,10 @@
 var appMgrApp = angular.module('appMgr', []);
 
 appMgrApp.controller('appListController',['$scope', 'applicationServices',  function ($scope, applicationService){
-	$scope.apps = [{"id":"1"},
-	               {"id":"21"}];
-	
+//	$scope.apps = [{"id":"1"},
+//	               {"id":"21"}];
+	$scope.apps = applicationService.getAllApps();
+	console.log($scope.apps);
   }]);
 
 appMgrApp.factory('applicationServices', 
