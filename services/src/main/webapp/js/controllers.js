5dbb9788d996f8cf4803c8cc999e0bcad402aec2
jcdwyer <jcdwyer@jcd-dev.net>
2014-12-11 19:52:18 -0600

    move controllers and services into separate '.js' modules

--- services/src/main/webapp/js/controllers.js
+++ services/src/main/webapp/js/controllers.js
@@ -1,7 +1,150 @@
-//angular.module('appMgr.controllers', []).controller('appListController', function($scope) {
-//	$scope.apps =  [];
-//});
+angular.module('appMgr.controllers', ['appMgr.services']);
 
-//appMgrApp.controller('appListController', 'applicationServices', function($scope, applicationServices) {
-//	$scope.apps =  applicationServices.getAllApps();
-//})
+angular.module('appMgr.controllers').controller('AppsController',
+		[ '$scope', 'Apps', function($scope, Apps) {
+
+			var init = function() {
+				console.info("init AppsController start");
+				$scope.newAppName = '';
+				$scope.apps = Apps.query();
+				console.info("init AppsController complete");
+			}
+
+			init();
+
+			$scope.saveAppButtonDisabled = function() {
+				return $scope.newAppName.length == 0;
+
+			}
+
+			$scope.saveNewApp = function() {
+				console.log("save");
+				Apps.save({
+					name : $scope.newAppName
+				}, function(data) {
+					Apps.query(function(data) {
+						console.log('saved');
+						$scope.apps = data;
+						$scope.newAppName = '';
+					}, function(err) {
+						alert('refresh failed');
+					});
+				}, function(data) {
+					alert("save failed")
+				})
+			};
+
+		} ]);
+
+angular.module('appMgr.controllers').controller('UsersController',
+		[ '$scope', 'Users', function($scope, Users) {
+
+			var init = function() {
+				console.info("init UsersController start");
+				$scope.users = Users.query();
+				console.info("init UsersController complete");
+			}
+
+			init();
+
+			$scope.newUser = function() {
+
+			};
+
+		} ]);
+
+angular.module('appMgr.controllers').controller(
+		'AppDetailController',
+		[ '$scope', '$routeParams', 'Apps',
+				function($scope, $routeParams, Apps) {
+
+					var init = function() {
+						console.info("init AppDetailController start");
+						$scope.appId = $routeParams.appId;
+						Apps.get({
+							appId : $scope.appId
+						}, function(data) {
+							$scope.app = data;
+							var x = data;
+							console.info("found app " + $scope.app.name);
+						}, function(error) {
+							alert("Could not find app" + error);
+						});
+
+						$scope.newProp = {
+							name : "",
+							environment : "",
+							type : "",
+							value : ""
+						}
+
+						console.info("init AppDetailController complete");
+					}
+
+					init();
+
+					$scope.saveProperty = function() {
+						console.info("save property");
+						$scope.newProp.appId = $scope.app.id;
+						var existingProps = $scope.app.properties;
+						existingProps[$scope.app.properties.length] = {
+							name : $scope.newProp.name,
+							environment : $scope.newProp.environment,
+							type : $scope.newProp.type,
+							value : $scope.newProp.value
+						};
+						$scope.newProp.name = "";
+						$scope.newProp.type = "";
+						$scope.newProp.value = "";
+
+					}
+
+					$scope.saveApp = function() {
+						if ($scope.app.id) {
+							Apps.update({
+								appId : $scope.app.id
+							}, $scope.app, function(data) {
+								$scope.app = data;
+								var x = data;
+								console.info("saved app " + $scope.app.name);
+							}, function(error) {
+								alert("Could not save app" + error.status);
+							});
+
+						} else {
+							Apps.save($scope.app, function(data) {
+								$scope.app = data;
+								var x = data;
+								console.info("Saved app " + $scope.app.name);
+							}, function(error) {
+								alert("Could not save app" + error.status);
+							});
+						}
+					}
+
+				} ]);
+
+angular.module('appMgr.controllers').controller(
+		'UserDetailController',
+		[ '$scope', '$routeParams', 'Users',
+				function($scope, $routeParams, Users) {
+
+					var init = function() {
+						console.info("init UserDetailController start");
+						$scope.userId = $routeParams.userId;
+						Users.get({
+							userId : $scope.userId
+						}, function(data) {
+							$scope.user = data;
+							var x = data;
+							console.info("found user " + $scope.user.name);
+						}, function(error) {
+							alert("Could not find user" + error);
+						});
+
+						console.info("init UserDetailController complete");
+					}
+
+					init();
+
+				} ]);
--- services/src/main/webapp/js/services.js
+++ services/src/main/webapp/js/services.js
@@ -1,10 +1,23 @@
-var appServices = angular.module('applicationService', ['ngResource']);
 
-appServices.factory('applicationServices', 
-		function($resource) {
-			return  {
-				getAllApps : function() {
-					return AppWS.listAllApplications();
-				}
-			};
-	});
+angular.module('appMgr.services', []);
+
+angular.module('appMgr.services').factory('Apps', function($resource) {
+	return $resource('http://127.0.0.1:8080/services/rest/apps/:appId', {
+		appId : '@id'
+	}, {
+		'update' : {
+			method : 'PUT',
+			params : {
+				appId : '@id'
+			}
+		}
+	}
+
+	); // Note the full endpoint address
+});
+
+angular.module('appMgr.services').factory('Users', function($resource) {
+	return $resource('http://127.0.0.1:8080/services/rest/users/:userId', {
+		userId : '@id'
+	}); // Note the full endpoint address
+});
